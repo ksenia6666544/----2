18eccd95a0644fd922ab71fe0bd1be12be9d4e50
ksenia6666544 <ksenia99937@gmail.com>
2025-05-24 09:42:04 +0300

    Add files via upload
    исправленная версия

--- лаб4/задание1.py
+++ лаб4/задание1.py
@@ -5,7 +5,7 @@ class Evergreen:
     Attributes:
         crown (str): Вид кроны дерева (например, овальная, конусообразная)
         cone (str): Вид шишки дерева (например, удлиненная, округлая)
-        tone (str): Цвет (оттенок) хвои (например, темный, светлый)
+        tone (str): Цвет (оттенок) хвои (например, тёмный, светлый)
     """
 
     def __init__(self, crown: str, cone: str, tone: str):
@@ -34,57 +34,60 @@ class Evergreen:
 
 class Pine(Evergreen):
     """
-    Дочерний класс для хвойных деревьев
+    Дочерний класс для сосны
 
     Attributes:
         crown (str): Вид кроны дерева (унаследовано)
         cone (str): Вид шишки дерева (унаследовано)
         tone (str): Цвет (оттенок) хвои (унаследовано)
-        height (int, float): Высота дерева
+        height (float): Высота дерева
     """
 
-    def __init__(self, crown: str, cone: str, tone: str, height: int | float):
+    def __init__(self, crown: str, cone: str, tone: str, height: float):
         super().__init__(crown, cone, tone)
         self.height = height
 
     @property
-    def height(self) -> (int, float):
+    def height(self) -> float:
         return self._height
 
     @height.setter
-    def height(self, value: (int, float)):
+    def height(self, value: float):
         if not isinstance(value, (int, float)):
-            raise TypeError('Высота дерева должна быть целым или вещественным числом')
+            raise TypeError('Высота дерева должна быть числовой')
         if value <= 0:
-            raise ValueError('Высота дерева должна быть положительным числом')
+            raise ValueError('Высота дерева должна быть положительной')
         self._height = value
 
     def __str__(self) -> str:
-        return f"{self.crown} {self.cone} {self.tone} Высота: {self._height:.2f} м"
+        return f"{self.crown} {self.cone} {self.tone}. Высота: {self.height:.2f} м"
+
+    def __repr__(self) -> str:
+        return f"Pine(crown={self.crown!r}, cone={self.cone!r}, tone={self.tone!r}, height={self.height:.2f})"
 
     def cut_down_tree(self) -> str:
         """
-        Срубает дерево
+        Метод для обозначения процесса рубки дерева.
 
         Returns:
-             str: Строка, обозначающая срубку дерева.
+            str: Сообщение о том, что дерево было срублено.
         """
         return "Дерево срублено"
 
     def plant_tree(self) -> str:
         """
-        Сажает дерево
+        Метод для обозначения посадки дерева.
 
         Returns:
-             str: Строка, обозначающая посадку дерева.
+            str: Сообщение о посадке дерева.
         """
         return "Дерево посажено"
 
     def decorate_tree(self) -> str:
         """
-        Украшает дерево.
+        Метод для украшения дерева.
 
         Returns:
-             str: Строка, обозначающее внешний вид дерева
+            str: Сообщение о украшении дерева.
         """
-        return "Дерево укашено"
+        return "Дерево украшено"
